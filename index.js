6903bd6eb6010dc3b78b87fc873372e3f6f16eb8
Aliyu Tunde <aliumuhammad2000@gmail.com>
2026-03-09 01:47:51 -0700

    fixed nav-menu and updated index.js

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const navBar = document.querySelector('#nav');
-const menuBar = document.querySelector('#menu');
+const navMenu = document.querySelector('.nav-menu');
+const menuBar = document.querySelector('.menu-bar');
+const closeIcon = document.querySelector('.icon-close');
+const hamburgerIcon = document.querySelector('.hamburger');
 
-navBar.addEventListener('click', () => {
-    if (menuBar.classList.contains('hidden')) {
-        menuBar.classList.remove('hidden');
-    } else {
-        menuBar.classList.add('hidden');
-    }
+menuBar.addEventListener('click', () => {
+    navMenu.classList.toggle('-translate-x-full');
+    closeIcon.classList.toggle('hidden');
+    hamburgerIcon.classList.toggle('hidden');
+    
+});
+
+const navLinks = document.querySelectorAll('.nav-menu a');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navMenu.classList.add('-translate-x-full');
+        closeIcon.classList.add('hidden');
+        hamburgerIcon.classList.remove('hidden');
+    });
 });
